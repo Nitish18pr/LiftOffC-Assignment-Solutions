63a1699ccd9ca6e28f38e3dee5ababeb6991d4f2
Nitish18pr <78494086+Nitish18pr@users.noreply.github.com>
2021-02-18 20:40:43 +0530

    Add files via upload

---
+++ file extension.c 1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+int arr[7],i,sum=0;
+printf("Enter any array having 7 elements: \n");
+for(i=0;i<7;i++){
+	scanf("&d",&arr[i]);
+	
+}
+if(arr[i]%2==0){sum=sum+arr[i];
+}
+}
+printf("\n sum of all the even elements and even position elements of the array is%d",sum);
+return 0;
+}
